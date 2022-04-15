fcd5e9d0aacb1ad86dbb8cd68a29f45b5df515a6
Sherpa <alex@sherpa.one>
2022-04-15 21:42:16 +0200

    add invoices

--- invoices_api/app.js
+++ invoices_api/app.js
@@ -2,17 +2,20 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const helmet = require('helmet');
+app.use(helmet());
 
-const indexRouter = require('./routes/index');
+const invoicesRouter = require('./routes/invoices');
 
 const app = express();
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', invoicesRouter);
 
 module.exports = app;
